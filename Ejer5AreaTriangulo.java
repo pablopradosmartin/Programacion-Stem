53d9a134b0db6796374a08239fb9f70d053cdb08
25740330h <pablopradosmartin@gmail.com>
2023-10-19 18:37:28 +0200

    Base para ejercicio 5

---
+++ Ejer5AreaTriangulo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejer5AreaTriangulo {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        System.out.println();
+        num = sc.nextInt();
+   
+   
+   
+   
+    }
+
+
+}
