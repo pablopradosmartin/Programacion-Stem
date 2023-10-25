f8e93c9fe9444424d083bff627db913778ba18d9
pablopradosmartin <116158569+pablopradosmartin@users.noreply.github.com>
2023-10-25 08:30:44 +0200

    Update Ejer5AreaTriangulo.java

--- Ejer5AreaTriangulo.java
+++ Ejer5AreaTriangulo.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Ejer5AreaTriangulo {
+public class Ej5AreaTriangulo {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
